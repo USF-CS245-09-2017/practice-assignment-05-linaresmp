3a5c7b7c868d5dd700006de1aabf86fc66379cf2
linaresmp <35755517+linaresmp@users.noreply.github.com>
2019-03-03 22:47:21 -0800

    Add files via upload

---
+++ QuickSort.java
@@ -0,0 +1,44 @@
+//QuickSort implements the SortingAlgorithm interface
+//QuickSort picks an element as pivot and partitions the given array around the picked pivot
+public class QuickSort implements SortingAlgorithm{
+
+public void sort(int arr[]){
+            int left = 0;
+            int right = arr.length-1;
+            quickSort(arr, left, right);
+      }
+
+
+int partition(int arr[], int left, int right)
+
+{
+      int i = left;
+      int j = right;
+      int tmp;
+      int pivot = arr[(left + right) / 2];
+      while (i <= j) {
+            while (arr[i] < pivot)
+                  i++;
+            while (arr[j] > pivot)
+                  j--;
+            if (i <= j) {
+                  tmp = arr[i];
+                  arr[i] = arr[j];
+                  arr[j] = tmp;
+                  i++;
+                  j--;
+            }
+      };
+      return i;
+}
+      
+
+ void quickSort(int arr[], int left, int right) {
+
+      int index = partition(arr, left, right);
+      if (left < index - 1)
+           quickSort(arr, left, index - 1);
+      if (index < right)
+            quickSort(arr, index, right);
+}
+}
